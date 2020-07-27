8f7e42b5d7b23d88368ad93c8b62c7dc437cbf78
MenshikovIA <62717964+MenshikovIA@users.noreply.github.com>
2020-07-27 18:19:02 +0300

    Update views.py

--- backend/quiz/api/views.py
+++ backend/quiz/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, generics
+from rest_framework import permissions
 
 from .serializers import QuizSerializer, QuestionSerializer, AnswerSerializer, UserAnswersListSerializer
 from ..models import Quiz, Question, Answer
@@ -7,10 +8,12 @@ from ..models import Quiz, Question, Answer
 class QuizViewSet(viewsets.ModelViewSet):
     queryset = Quiz.objects.all().order_by('-start_date')
     serializer_class = QuizSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class QuestionAPIView(generics.ListAPIView):
     serializer_class = QuestionSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         pk = self.kwargs.get('pk')
@@ -24,6 +27,7 @@ class AnswerCreateAPIView(generics.CreateAPIView):
 
 class UserAnswers(generics.ListAPIView):
     serializer_class = UserAnswersListSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         pk = self.kwargs.get('pk')
